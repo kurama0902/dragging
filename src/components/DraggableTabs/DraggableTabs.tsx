29b2fadd0702559509a8da3706c2daa064213dbf
kurama0902 <creepysimbaplay@gmail.com>
2024-06-19 19:09:32 +0300

    test commit

--- src/components/DraggableTabs/DraggableTabs.tsx
+++ src/components/DraggableTabs/DraggableTabs.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 
